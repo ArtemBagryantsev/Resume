c763c486130588749d109c23f37b5679252bbe9d
ArtemBagryantsev <artembagrjancev@gmail.com>
2023-06-04 11:44:05 +0400

    Second commit

--- main/forms.py
+++ main/forms.py
@@ -7,17 +7,14 @@ class ContactForm(forms.ModelForm):
 	name = forms.CharField(max_length=100, required=True,
 		widget=forms.TextInput(attrs={
 			'placeholder': '*Full name..',
-			'class': 'form-control'
 			}))
 	email = forms.EmailField(max_length=254, required=True, 
 		widget=forms.TextInput(attrs={
 			'placeholder': '*Email..',
-			'class': 'form-control'
 			}))
 	message = forms.CharField(max_length=1000, required=True, 
 		widget=forms.Textarea(attrs={
 			'placeholder': '*Message..',
-			'class': 'form-control',
 			'rows': 6,
 			}))
 
