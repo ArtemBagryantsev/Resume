226c8f6fa7c5aa3dfbe1f641366994c5dbe97e19
ArtemBagryantsev <artembagrjancev@gmail.com>
2023-06-08 09:36:19 +0400

    Fifth commit

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
-    path('contact', views.ContactView.as_view(), name='contact'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
     path('portfolio/', views.PortfolioView.as_view(), name='portfolios'),
     path('portfolio/<slug:slug>', views.PortfolioDetailView.as_view(), name='portfolio'),
     path('blog/', views.BlogView.as_view(), name='blogs'),
